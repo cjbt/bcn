bbde90d4c608ff0483bf6a0280b837f78cd1047d
Cecil John Tantay <cjbtantay@gmail.com>
2018-08-22 08:31:25 -0700

    added mobile first responsive features. still a lot of work to do with the navbar more menu and above 600px

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// topnav menu //
+function menu() {
+  var x = document.getElementById("menu-links");
+  if (x.style.display === "none") {
+      x.style.display = "block";
+  } else {
+      x.style.display = "none";
+  }
+}
+// topnav menu end //
